6a8fb7dd371d36cd3ac4b09374aac14445e11989
huanghao20011127 <103709183+huanghao20011127@users.noreply.github.com>
2022-04-15 18:07:08 +0800

    测试pull

--- springMVCDemo1/src/main/java/cn/edu/xyc/controller/HelloWorldController.java
+++ springMVCDemo1/src/main/java/cn/edu/xyc/controller/HelloWorldController.java
@@ -12,7 +12,7 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String index(){
-        return "index";
+        return "index删除Pull";
     }
 
     @RequestMapping("/helloWorld")
